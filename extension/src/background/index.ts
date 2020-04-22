acb311cad05efc03b9eaed3b71e25491fa1e7074
Ismaïl MOURTADA <ismail.mourtada@gmail.com>
2020-04-22 20:49:02 +0200

    feat(feature-10): minimal communicating bs and cs

--- extension/src/background/index.ts
+++ extension/src/background/index.ts
@@ -1,4 +1,5 @@
 import { Poller } from "./poller";
+import { browser, Runtime } from "webextension-polyfill-ts";
 
 const log = require("debug")("ext:background");
 
@@ -8,3 +9,16 @@ async function polling() {
 }
 
 Poller.getInstance(polling);
+
+var portFromCS: Runtime.Port;
+
+const connected = (p: Runtime.Port) => {
+    portFromCS = p;
+    portFromCS.postMessage({ greeting: "Sending message to CS" });
+    portFromCS.onMessage.addListener((m: any) => {
+        console.log("Message from CS");
+        console.log(m.greeting);
+    });
+};
+
+browser.runtime.onConnect.addListener(connected);
--- extension/src/contentscript/index.ts
+++ extension/src/contentscript/index.ts
@@ -1,1 +1,16 @@
 console.log(`Loaded into page`);
+
+import { browser } from "webextension-polyfill-ts";
+
+const myPort = browser.runtime.connect(undefined, { name: "PORT-CS" });
+myPort.postMessage({ greeting: "This is the content script" });
+
+myPort.onMessage.addListener((message: any) => {
+    console.log("Message received from BG script");
+    console.log(message.greeting);
+});
+
+document.body.addEventListener("click", () => {
+    console.log("click");
+    myPort.postMessage({ greeting: "clickCS" });
+});
