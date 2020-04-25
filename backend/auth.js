d6b9b2cdfe27399c438c74d7db0b470bc219e262
gjgd <gjgd@protonmail.com>
2020-04-26 01:35:14 +0900

    chore: separate auth from other handlers

---
+++ backend/auth.js
@@ -0,0 +1,19 @@
+// In case we want to have authenticated socket connections
+module.exports.auth = async (event, context) => {
+    // return policy statement that allows to invoke the connect function.
+    // in a real world application, you'd verify that the header in the event
+    // object actually corresponds to a user, and return an appropriate statement accordingly
+    return {
+        principalId: "user",
+        policyDocument: {
+            Version: "2012-10-17",
+            Statement: [
+                {
+                    Action: "execute-api:Invoke",
+                    Effect: "Allow",
+                    Resource: event.methodArn,
+                },
+            ],
+        },
+    };
+};
--- backend/handler.js
+++ backend/handler.js
@@ -33,22 +33,3 @@ module.exports.default = async (event, context, cb) => {
         body: "Sent.",
     });
 };
-
-module.exports.auth = async (event, context) => {
-    // return policy statement that allows to invoke the connect function.
-    // in a real world application, you'd verify that the header in the event
-    // object actually corresponds to a user, and return an appropriate statement accordingly
-    return {
-        principalId: "user",
-        policyDocument: {
-            Version: "2012-10-17",
-            Statement: [
-                {
-                    Action: "execute-api:Invoke",
-                    Effect: "Allow",
-                    Resource: event.methodArn,
-                },
-            ],
-        },
-    };
-};
