84e013385c33d9f8888a067939d1158e27bb33ab
Cyril Duchon-Doris <cyril@myjobglasses.com>
2020-05-01 19:27:20 +0200

    fix: use MessageType from enum

--- extension/src/contentscript/index.ts
+++ extension/src/contentscript/index.ts
@@ -1,10 +1,11 @@
 import { browser } from "webextension-polyfill-ts";
+import { MessageType } from "../types";
 
 console.log(`[CS] Loading WatchWithMe Extension content script`);
 
 const csPort = browser.runtime.connect(undefined, { name: "PORT-CS" });
 csPort.postMessage({
-    type: "debugMessage",
+    type: MessageType.DEBUG_MESSAGE,
     message: "[CS] This is the content script",
 });
 
@@ -13,7 +14,7 @@ if (roomId) {
     console.log(
         `[CS] detected roomId ${roomId} from URL param, notifying background script`
     );
-    csPort.postMessage({ type: "changeRoom", roomId });
+    csPort.postMessage({ type: MessageType.CHANGE_ROOM, roomId });
 } else {
     console.log("[CS] Invalid URL missing roomID");
 }
