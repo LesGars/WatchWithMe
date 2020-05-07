acdf3184dba2ac6f14e2f81cc46fafeca9d14da3
Ismaïl MOURTADA <ismail.mourtada@gmail.com>
2020-05-07 23:16:58 +0200

    feat(33): send previous room id via websocket

--- extension/env/development.env.js
+++ extension/env/development.env.js
@@ -1,8 +1,10 @@
-var merge = require('webpack-merge')
-var prodEnv = require('./production.env')
+var merge = require("webpack-merge");
+var prodEnv = require("./production.env");
+var common = require("./common");
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  HOST: '"127.0.0.1:8000"',
-  USE_API_MOCK: 'false'
-})
+    NODE_ENV: '"development"',
+    HOST: '"127.0.0.1:8000"',
+    USE_API_MOCK: "false",
+    ...common,
+});
