3ff9d7b80db7f14f02ecce2d0c62e011546c4494
Ismaïl MOURTADA <ismail.mourtada@gmail.com>
2020-05-12 22:52:24 +0200

    feat(59): remove the common import from production.env

--- extension/env/production.env.js
+++ extension/env/production.env.js
@@ -1,8 +1,6 @@
-var common = require("./common").prod;
-
 module.exports = {
     NODE_ENV: '"production"',
     HOST: '"prod-host:80"',
     USE_API_MOCK: "false",
-    ...common,
+    WS_URL: '""',
 };
